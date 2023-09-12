79f0e886c7828867d90572e41e2de237d5298771
yuuhalca <h.c.nicol@gmail.com>
2023-09-12 23:29:57 +0900

    9-12

--- page.php
+++ page.php
@@ -18,7 +18,10 @@ ksort($pages);
 $num = 0;
 foreach ( $pages as $page ) {
 	$data = "<div class='page' id='{$page[1]}'><div class='page_content'>";
-	$data .= $page[0];	
+	$data .= $page[0]."<br>";	
+	if($page[1] == "inquiry"){
+		$data .= do_shortcode('[contact-form-7 id="dd3bb5b" title="コンタクトフォーム 1"]');
+	}
 	$data .= "</div></div>";
 	echo $data;
 	$num++;
